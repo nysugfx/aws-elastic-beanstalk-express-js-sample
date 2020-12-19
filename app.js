156c9332d140cf5e23aad61e106cf0d9ea412acc
nysugfx <nysu.gfx@gmail.com>
2020-12-19 15:35:48 -0500

    full pipeline test

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const port = 8080;
 
-app.get('/', (req, res) => res.send('Seth Is Shot'));
+app.get('/', (req, res) => res.send('Seth Is Shot, bitch'));
 
 app.listen(port);
 console.log(`App running on http://localhost:${port}`);
